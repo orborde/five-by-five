0fac3e2426c965ad44f581187a4c3179b2c29b8b
William Ehlhardt <williamehlhardt@gmail.com>
2018-07-05 01:32:21 -0700

    dictionary

---
+++ dict.py
@@ -0,0 +1,11 @@
+WORDSFILE='/usr/share/dict/words'
+
+WORDS = set()
+with open(WORDSFILE) as f:
+    for w in f:
+        w = w.strip()
+        if len(w) == 5 and w.isalpha():
+            WORDS.add(w.lower())
+
+if __name__ == '__main__':
+    print len(WORDS), 'words loaded'

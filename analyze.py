50a4c10b10518b36c01c949f167cc07013dab946
William Ehlhardt <williamehlhardt@gmail.com>
2018-07-05 02:01:29 -0700

    calculate distinct reachable knowledge states (ish)

--- analyze.py
+++ analyze.py
@@ -39,10 +39,19 @@ def group(possibilities, guess):
         cts[clue(p, guess)].add(p)
     return cts
 
+kstates = set()
 sortedwords = sorted(WORDS)
 for w in sortedwords:
     groups = group(sortedwords, w)
     print w, sum(len(v) for k, v in groups.items() if k > 0),
+    ak = 0
     for v in sorted(groups.keys()):
         print '{}:{}'.format(v, len(groups[v])),
-    print
+        kstate = frozenset(groups[v])
+        if kstate not in kstates:
+            ak += 1
+            kstates.add(kstate)
+    print '({} kstates so far +{} this round)'.format(len(kstates), ak)
+
+print len(kstates), 'total kstates'
+print len(kstates)/float(len(WORDS)), 'kstates per word?'

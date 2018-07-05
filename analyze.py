ab42fc7cb26a178b2d8c37dad2348f27982b3b48
William Ehlhardt <williamehlhardt@gmail.com>
2018-07-05 03:22:44 -0700

    refactor

--- analyze.py
+++ analyze.py
@@ -5,36 +5,7 @@ import resource
 import time
 
 from dict import WORDS
-
-import doctest
-def load_tests(loader, tests, ignore):
-    tests.addTests(doctest.DocTestSuite())
-    return tests
-
-def count(s):
-    s = s.upper()
-    cts = collections.defaultdict(int)
-    for c in s:
-        cts[c] += 1
-    return cts
-
-def clue(secret, guess):
-    """
-    >>> clue('QUERY', 'BACON')
-    0
-    >>> clue('TRYST', 'STRYT')
-    5
-    >>> clue('TRYST', 'TRIES')
-    3
-    >>> clue('BACON', 'HANDY')
-    2
-    """
-    secretcts = count(secret)
-    guesscts  = count(guess)
-    sm = 0
-    for key in set(itertools.chain(secretcts, guesscts)):
-        sm += min(secretcts[key], guesscts[key])
-    return sm
+from game import *
 
 def anagramkey(word):
     return ''.join(sorted(word))
---
+++ game.py
@@ -0,0 +1,32 @@
+import collections
+import itertools
+
+import doctest
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite())
+    return tests
+
+def count(s):
+    s = s.upper()
+    cts = collections.defaultdict(int)
+    for c in s:
+        cts[c] += 1
+    return cts
+
+def clue(secret, guess):
+    """
+    >>> clue('QUERY', 'BACON')
+    0
+    >>> clue('TRYST', 'STRYT')
+    5
+    >>> clue('TRYST', 'TRIES')
+    3
+    >>> clue('BACON', 'HANDY')
+    2
+    """
+    secretcts = count(secret)
+    guesscts  = count(guess)
+    sm = 0
+    for key in set(itertools.chain(secretcts, guesscts)):
+        sm += min(secretcts[key], guesscts[key])
+    return sm

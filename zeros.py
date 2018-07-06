f731935dd30440fe81c4c873b8edb5a9e6ab64e5
William Ehlhardt <williamehlhardt@gmail.com>
2018-07-06 01:33:30 -0700

    calculate best words for numbers

--- zeros.py
+++ zeros.py
@@ -8,6 +8,7 @@ SORTEDWORDS = sorted(WORDS)
 
 sms = collections.defaultdict(int)
 ct  = collections.defaultdict(int)
+mins = collections.defaultdict(lambda: (99999999, "zzzzz"))
 for w in SORTEDWORDS:
     groups = group(SORTEDWORDS, w)
     print w,
@@ -15,10 +16,12 @@ for w in SORTEDWORDS:
         print '{}:{}'.format(v, len(groups[v])),
         sms[v] += len(groups[v])
         ct[v] += 1
+        mins[v] = min(mins[v], (len(groups[v]), w))
     print
 
 print len(SORTEDWORDS),
 print 'averages:'
 for k in sorted(sms.keys()):
-    print k, ':', float(sms[k]) / ct[k]
+    print k, ':', float(sms[k]) / ct[k], mins[k],
+    print sorted(((k, len(v)) for k,v in group(SORTEDWORDS, mins[k][1]).items()))
 print 'global narrowing:', float(sum(sms.values()))/len(SORTEDWORDS)

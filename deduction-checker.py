d3890494d6d9c920f411472aa27f49756987734b
William Ehlhardt <williamehlhardt@gmail.com>
2018-07-07 23:15:57 -0700

    print out possible letters

--- deduction-checker.py
+++ deduction-checker.py
@@ -13,7 +13,8 @@ for line in sys.stdin:
     clues.append( (w,n) )
 
     maybewords = [word for word in WORDS if all(clue(word, cw) == cn for cw,cn in clues)]
-    print w, n, len(maybewords),
+    letters = ''.join(sorted(set(itertools.chain(*maybewords))))
+    print w, n, len(maybewords), letters, len(letters),
     if len(maybewords) <= 10:
         print sorted(maybewords),
     print

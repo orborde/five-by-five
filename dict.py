4d0dfc4c3f910784d2b38878ca762113c6dd2b9b
William Ehlhardt <williamehlhardt@gmail.com>
2018-09-05 00:56:09 -0700

    anagram stats

--- dict.py
+++ dict.py
@@ -1,3 +1,5 @@
+import collections
+
 WORDSFILE='/usr/share/dict/words'
 
 WORDS = set()
@@ -9,5 +11,19 @@ with open(WORDSFILE) as f:
 
 SORTEDWORDS = sorted(WORDS)
 
+def anakey(w):
+    return ''.join(sorted(w.upper()))
+
+ANAGRAMS = collections.defaultdict(list)
+for w in WORDS:
+    ANAGRAMS[anakey(w)].append(w)
+ANAGRAMS = dict(ANAGRAMS)
+
 if __name__ == '__main__':
     print len(WORDS), 'words loaded'
+    print len(ANAGRAMS), 'anagram families'
+    anafams = sorted(
+        ANAGRAMS.keys(),
+        key=lambda k: len(ANAGRAMS[k]),
+        reverse=True)
+    print 'Largest family:', sorted(ANAGRAMS[anafams[0]])

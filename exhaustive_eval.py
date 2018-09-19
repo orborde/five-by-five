0006af5b16cfe0e0bf7b0d877c99d7c90cf69a2d
William Ehlhardt <williamehlhardt@gmail.com>
2018-09-19 02:09:02 -0700

    formatting

--- exhaustive_eval.py
+++ exhaustive_eval.py
@@ -7,5 +7,5 @@ from splitbot import SplitBot
 
 for i,w in enumerate(SORTEDWORDS):
     guesses = drivers.play_with(SplitBot(), word=w, verbose=False)
-    print i, '%.0f%%'%(100.0 * (i+1) / len(SORTEDWORDS)), w,
+    print i, '%.0f%%'%(100.0 * (i+1) / len(SORTEDWORDS)), w, ':',
     print ' '.join(guesses)
